8b458b49f5c23f098e11f3ed274a2952ae043a6a
Joshua Quarcoo <78474714+joshuaquarcoonii1@users.noreply.github.com>
2025-01-30 11:39:21 +0000

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -473,7 +473,7 @@ app.post('/api/reports/:id/assign', async (req, res) => {
 // });
 
 // Start the server
-const PORT =5000;
+const PORT =process.env.PORT||5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
